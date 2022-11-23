ba9f0774a2b7992fc35ff4188b3843a5a11b9e1a
Lydiiamm <lydiiamorenojimenez@gmail.com>
2022-11-23 13:41:38 +0100

    Spring Boot application, connection with DB

---
+++ first_version/src/main/java/com/shop_java_school/first_version/FirstVersionApplication.java
@@ -0,0 +1,13 @@
+package com.shop_java_school.first_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstVersionApplication.class, args);
+	}
+
+}
---
+++ first_version/src/test/java/com/shop_java_school/first_version/FirstVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop_java_school.first_version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
